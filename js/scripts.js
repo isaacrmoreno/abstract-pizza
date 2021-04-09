194e1764d01cf3570999471abada40a8ab669e09
Isaac Moreno <isaacmoreno@Isaacs-Mac-mini.local>
2021-04-09 10:29:56 -0700

    add some html and css styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,54 @@
+// Business logic for pizza
 function Pizza(size, toppings) {
   this.size = size;
   this.toppings = toppings;
 }
 
-let testPizza = new Pizza("Large", "Pepperoni") 
-console.log('I would like a ' + testPizza.size + " " + testPizza.toppings + ' please.')
+let testPizza = new Pizza("large", "pepperoni") 
+// console.log("I would like a " + testPizza.size + " " + testPizza.toppings + " please.")
 
+
+
+
+
+
+
+
+
+
+// Pizza.prototype.addToppings = function(topping) {
+//   this.topping[topping.firstTopping] = topping;
+// };
+
+// // Business logic for toppings
+
+//   function Toppings(firstTopping, secondTopping, thirdTopping) {
+//     this.firstTopping = firstTopping;
+//     this.secondTopping = secondTopping;
+//     this.thirdTopping = thirdTopping;
+//   }
+
+//   let pizza = new Pizza();
+//   let isaacPizza = new Toppings("sausage", "black olives")
+
+//   Pizza.addToppings(isaacPizza);
+
+// Business Logic for AddressBook ---------
+function AddressBook() {
+  this.contacts = {};
+}
+
+AddressBook.prototype.addContact = function(contact) {
+  this.contacts[contact.firstName] = contact;
+};
+
+// Business Logic for Contacts ---------
+function Contact(firstName, lastName, phoneNumber) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.phoneNumber = phoneNumber;
+}
+
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+};
