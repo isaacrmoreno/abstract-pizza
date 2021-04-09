350bbff3e69a93c46defc3627c8b12eaf4ade51c
Isaac Moreno <isaacmoreno@Isaacs-Mac-mini.local>
2021-04-09 16:45:52 -0700

    be the last commit for the day

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,84 @@
 // Business logic for pizza
 
 function Pizza() {
-  this.size = {};
   this.topping = {};
 }
 
-Pizza.prototype.chooseSize = function(size) {
-  this.size[size.xxxxx] = size
+Pizza.prototype.addTopping = function(topping) {
+  this.topping[topping.pepperoni] = topping;
+};
+
+function Topping(toppingOne, toppingTwo, toppingThree) {
+  this.toppingOne = toppingOne;
+  this.toppingTwo = toppingTwo;
+  this.toppingThree = toppingThree;
 }
 
-function Pizza(size, topping) { // this is linked with whats on line 35
-  this.size = size;
-  this.topping = topping;
-}
+// function Topping(pepperoni, mozzarellaCheese, blackOlives, jalapenos, basil) {
+//   this.pepperoni = pepperoni;
+//   this.mozzarellaCheese = mozzarellaCheese;
+//   this.blackOlives = blackOlives;
+//   this.jalapenos = jalapenos;
+//   this.basil = basil;
+// }
 
-function Size(small, medium, large) {
-  this.small = small;
-  this.medium = medium; 
-  this.large = large;
-}
 
-function Topping(pepperoni, mozzarellaCheese, blackOlives, jalapenos, basil) {
-  this.pepperoni = pepperoni;
-  this.mozzarellaCheese = mozzarellaCheese;
-  this.blackOlives = blackOlives;
-  this.jalapenos = jalapenos;
-  this.basil = basil;
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function Pizza() {
+//   this.size = {};
+//   this.topping = {};
+// }
 
 Pizza.prototype.chooseTopping = function(topping) {
   this.topping[topping.zzzz] = topping
 }
 
+// Pizza.prototype.chooseSize = function(size) {
+//   this.size[size.xxxxx] = size
+// }
+
+function Pizza(size, topping) { 
+  this.size = size;
+  this.topping = topping;
+}
+
 let testpizza1 = new Pizza("small", "pepperoni")
 
-let pizza.chooseSize(Size) // This is not correct. reread to yourself when you get back from your walk.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // test pizza 1?
@@ -43,32 +87,6 @@ let pizza.chooseSize(Size) // This is not correct. reread to yourself when you g
 // pizza.topping = ["Pepperoni", "Mozzarella Cheese", "Black Olives", "Jalapenos", "Basil"]  
 
 // console.log(pizza.topping[0])
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // test pizza 2
 // let testPizza = new Pizza("large", "pepperoni") 
@@ -103,11 +121,8 @@ let pizza.chooseSize(Size) // This is not correct. reread to yourself when you g
 // let pizza = [size, topping]
 // console.log(pizza) -- returns undefined whenever I want to add dot notation after (pizza) to nest deeper into array? // rename topping variable or name within {} to avoid confusion. 
 
-// use size to define cost
-// small: $7, medium: $10, large: $13
 
 // User Interface Logic 
-
 // $(document).ready(function(){
 //   event.preventDefault()
 // })
