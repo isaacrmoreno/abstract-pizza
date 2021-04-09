a5c88e9c5f61f079f470d4e4f454e0ecc7e535c4
Isaac Moreno <isaacmoreno@Isaacs-Mac-mini.local>
2021-04-09 11:56:53 -0700

    add checkbox buttons to html page

--- js/scripts.js
+++ js/scripts.js
@@ -13,7 +13,7 @@ let testPizza = new Pizza("large", "pepperoni")
 console.log("I would like a " + testPizza.size + " " + "pizza with " + testPizza.toppings + " please.")
 
 // use size to define cost
-
+// small: $7, medium: $10, large: $13
 
 // size 
 let small = {size: "small"}
@@ -27,10 +27,17 @@ let pepperoni = {topping: "pepperoni"}
 let mozzarellaCheese = {topping: "mozzarella cheese"}
 let blackOlives = {topping: "black olives"}
 let jalapenos = {topping: "jalapenos"}
-let basil = {topping: "sausage"}
+let basil = {topping: "basil"}
 
-let toppings = {toppings: [pepperoni, mozzarellaCheese, blackOlives, jalapenos, basil]}
+let topping = {topping: [pepperoni, mozzarellaCheese, blackOlives, jalapenos, basil]}
 
-let pizza = [size, toppings]
+let pizza = [size, topping]
+console.log(pizza) // returns undefined whenever I want to add .notation after pizza to nest deeper into array? // rename topping variable or name within {} to avoid confusion. 
 
-console.log(pizza)
+
+// User Interface Logic 
+
+$(document).ready(function({
+  event.preventDefault()
+  
+}))
